37b725b92128e3d94b87d83c1aad92629671dc6f
Jordan Earls <earlz@earlz.net>
2017-11-29 17:04:35 -0500

    Add more modrm tests

--- tests/mov_tests.cpp
+++ tests/mov_tests.cpp
@@ -40,11 +40,26 @@ TEST_CASE("modrm ebp+eax*2", "[modrm]") {
     check.SetReg32(EAX, 4);
     test.Compare(check);
 }
-TEST_CASE("modrm ebp+4*2", "[modrm]") {
+TEST_CASE("modrm ebp+disp*scale", "[modrm]") {
     x86Tester test;
     test.Run(
 "mov ebp, _tmp\n"
 "mov ebx, [ebp + 4 * 2]\n" //resolves to _tmp + 4 * 2
+"jmp _end\n"
+"_tmp: dw 0, 0, 0, 0\n"
+"dd 0x12345678\n");
+    x86Checkpoint check = test.LoadCheckpoint();
+    //load checkpoint after running so that we can just set the registers we care about the results of
+    check.SetReg32(EBX, 0x12345678);
+    test.Compare(check);
+}
+
+TEST_CASE("modrm16 si+di+disp", "[modrm]") {
+    x86Tester test;
+    test.Run(
+"mov bx, _tmp\n"
+"mov si, 6\n"
+"mov ebx, [bx + si + 2]\n" //resolves to _tmp + 6 + 2
 "jmp _end\n"
 "_tmp: dw 0, 0, 0, 0\n"
 "dd 0x12345678\n");
