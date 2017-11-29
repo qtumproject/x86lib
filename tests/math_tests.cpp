7358767470333a8e8c2b2efcb1ca7f9484bc392b
Jordan Earls <earlz@earlz.net>
2017-11-29 18:19:26 -0500

    Add math test file

---
+++ tests/math_tests.cpp
@@ -0,0 +1,10 @@
+#include "catch.hpp"
+
+#define private public
+#define protected public
+#include "x86lib.h"
+#include "x86lib_internal.h"
+#include "x86test.h"
+
+using namespace x86Lib;
+using namespace std;
