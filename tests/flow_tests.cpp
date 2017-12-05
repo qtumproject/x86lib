57ff157aae455f70e926c4f9e9017a611a61dfde
Jordan Earls <earlz@earlz.net>
2017-12-05 16:18:00 -0500

    Add jmp_relW test

--- tests/flow_tests.cpp
+++ tests/flow_tests.cpp
@@ -1,3 +1,39 @@
 #include "x86test.h"
 
-
+TEST_CASE("op_jmp_relW", "[flow]"){
+    x86Tester test;
+    x86Checkpoint check = test.LoadCheckpoint();
+    test.Assemble(
+"jmp WORD test1\n" //0: 66 e9 xx xx
+"nop\n" //4: 90
+"test4:\n"
+"nop\n" //5: 90
+"test2:\n"
+"jmp DWORD test4\n" //6: e9 xx xx xx xx
+"nop\n" //B: 90
+"nop\n" //C: 90
+"test1:\n"
+"jmp DWORD test3\n" //D: e9 xx xx xx xx
+"test3:\n"
+"nop\n" //12: 90
+"jmp WORD test2\n" //13: 66 e9 xx xx
+); 
+    test.Run(1);
+    check.SetEIP(0x1000 + 0x0D);
+    test.Compare(check, true);
+    test.Run(1);
+    check.SetEIP(0x1000 + 0x12);
+    test.Compare(check, true);
+    test.Run(1);
+    check.SetEIP(0x1000 + 0x13);
+    test.Compare(check, true);
+    test.Run(1);
+    check.SetEIP(0x1000 + 0x06);
+    test.Compare(check, true);
+    test.Run(1);
+    check.SetEIP(0x1000 + 0x05);
+    test.Compare(check, true);
+    test.Run(1);
+    check.SetEIP(0x1000 + 0x06);
+    test.Compare(check, true);
+}
--- tests/mov_tests.cpp
+++ tests/mov_tests.cpp
@@ -1,13 +1,4 @@
-#include "catch.hpp"
-
-#define private public
-#define protected public
-#include "x86lib.h"
-#include "x86lib_internal.h"
 #include "x86test.h"
-
-using namespace x86Lib;
-using namespace std;
 
 TEST_CASE("mov_eax_imm32", "[mov]") {
     x86Tester test;
